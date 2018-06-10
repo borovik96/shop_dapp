b0071c211fbd50c2a71e7686f73d6bdef0bb1083
mixzenin <mix.zenin.ifmo@yandex.ru>
2018-06-10 22:39:35 +0300

    added initiateTrade func

--- shop.sol
+++ shop.sol
@@ -16,10 +16,9 @@ contract Ownable {
 }
 
 contract Shop is Ownable{
-  struct trade {
+  struct Trade {
     address sellerAddr;
-    bytes32 sigSeller;
-    bytes32 sigBuyer;
+    address buyerAddr;
     bytes32 dataHash;
     uint sum;
   }
@@ -27,7 +26,7 @@ contract Shop is Ownable{
   uint commission = 10; // комиссия в %, умноженной на 100, например чтобы выставить комиссию 0.01% - указываем "1"
   uint commissionAmount; // размер доступных для вывода средств в wei
 
-  event Trade();
+  event InitiateTrade(address sellerAddr, address buyerAddr, bytes32 dataHash, uint sum);
 
   function Shop(address[] _administrators) {
     for(uint i = 0; i < _administrators.length(); i++) {
@@ -37,43 +36,53 @@ contract Shop is Ownable{
   }
 
   function initiateTrade(
-    address sellerAddr,
-    bytes32 sigSeller,
-    bytes32 sigBuyer,
-    bytes32 dataHash,
-    uint sum
-   ) {
+    address _sellerAddr,
+    uint _vSeller,
+    bytes32 _rSeller,
+    bytes32 _sSeller,
+    uint _vBuyer,
+    bytes32 _rBuyer,
+    bytes32 _sBuyer,
+    bytes32 _dataHash,
+    uint _sum
+   ) payable public {
+     require(msg.value == _sum);
+     require(ecrecover(_dataHash, uint8(_vSeller), _rSeller, _sSeller) == _sellerAddr); // проверяем корректность подписи продавца
+     require(ecrecover(_dataHash, uint8(_vBuyer), _rBuyer, _sBuyer) == msg.sender); // проверяем корректность подписи покупателя
+     require(trades[_dataHash] == 0);
+     Trade trade = new Trade(_sellerAddr, msg.sender, _dataHash, _sum);
+     trades[_dataHash] = trade;
+     InitiateTrade(_sellerAddr, msg.sender, _dataHash, _sum);
+  }
+
+  function resolveTrade(bytes32 tradeId) public{
 
   }
 
-  function resolveTrade(bytes32 tradeId) {
+  function rejectTrade(bytes32 tradeId) public{
 
   }
 
-  function rejectTrade(bytes32 tradeId) {
-
-  }
-
-  function withdrawCommission(address destination) onlyOwner {
+  function withdrawCommission(address destination) public onlyOwner {
     destination.transfer(commissionAmount);
     commissionAmount = 0;
   }
 
-  function setCommission(uint _commission) { // комиссия в %, умноженной на 100, например чтобы выставить комиссию 0.01% - указываем "1"
+  function setCommission(uint _commission) onlyOwner public { // комиссия в %, умноженной на 100, например чтобы выставить комиссию 0.01% - указываем "1"
     require(_commission > 0);
     commission = _commission;
   }
 
-  function getCommissionAmount() onlyOwner {
+  function getCommissionAmount() public onlyOwner {
     return commissionAmount;
   }
 
-  function addAdministrator(address _administrator) onlyOwner {
+  function addAdministrator(address _administrator) public onlyOwner {
     require(!administrators[_administrator]);
     administrators[_administrator] = true;
   }
 
-  function deleteAdminitstrator(address _administrator) onlyOwner {
+  function deleteAdminitstrator(address _administrator) public onlyOwner {
     require(administrators[_administrator]);
     administrators[_administrator] = false;
   }
